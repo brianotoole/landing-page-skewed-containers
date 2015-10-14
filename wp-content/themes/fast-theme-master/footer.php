ae371b4fff20443bc6f6e3fe518c42a9f1ee005f
brianotoole <botoole518@gmail.com>
2015-10-14 19:02:10 -0400

    skews

--- wp-content/themes/fast-theme-master/footer.php
+++ wp-content/themes/fast-theme-master/footer.php
@@ -10,6 +10,14 @@
 
 </div><!-- /.wrapper -->
 
+  <div class="container-full footer">
+    <div class="row entry-content">
+      <div class="col-sm-12 copyright">
+        <p>&copy; <?php echo date('Y'); ?> | <?php bloginfo('site_name'); ?></p>
+      </div>
+    </div>
+  </div>
+    
 <?php wp_footer(); ?>
 
 </body>
