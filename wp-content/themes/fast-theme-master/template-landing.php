67e16e286256b1f8c7616589ce5efbe83afad37d
brianotoole <botoole518@gmail.com>
2015-10-15 13:03:24 -0400

    new skew containers

--- wp-content/themes/fast-theme-master/template-landing.php
+++ wp-content/themes/fast-theme-master/template-landing.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
   <div class="landing">
   
-    <div class="container-full hero skew-left">
+    <div class="container-full hero">
       <div class="row">
         <div class="col-sm-12">
 	        <div class="logo">
@@ -21,39 +21,16 @@ get_header(); ?>
       </div><!-- /.row-->
     </div><!-- /.section-1-->
     
-    <div class="container-full section one skew-left">
-      <div class="row content">
+    <div class="container-full section skew one">
+      <div class="row entry-content">
         <div class="col-sm-12">
           <h1>Heading One</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
         </div>
       </div>
     </div>
 
-    <div class="container-full section-3 skew-right">
-      <div class="entry-content">
-        <div class="row">
-          <div class="col-sm-12">
-            <h1>Another Heading</h1>
-          </div><!-- /.col-->
-        </div><!-- /.row-->
-        <div class="row">
-          <div class="col-sm-4">
-            <h3>We.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-          </div><!-- /.col-->
-          <div class="col-sm-4">
-            <h3>The.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-          </div><!-- /.col-->
-          <div class="col-sm-4">
-            <h3>Best.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-          </div><!-- /.col-->
-        </div><!-- /.row-->
-      </div><!-- /.entry-content -->
-    </div><!-- /.container-fluid -->
 
-    
   </div><!-- /.landing -->
 
 <?php get_footer(); ?>
